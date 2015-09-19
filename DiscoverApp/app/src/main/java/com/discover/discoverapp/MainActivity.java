de594bb7ba1f18afaed5347277cc7de7b6f4e314
Joy <joyhsu0504@gmail.com>
2015-09-19 08:33:45 -0400

    added swipe and image change

--- DiscoverApp/app/src/main/java/com/discover/discoverapp/MainActivity.java
+++ DiscoverApp/app/src/main/java/com/discover/discoverapp/MainActivity.java
@@ -1,18 +1,59 @@
 package com.discover.discoverapp;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
 
 public class MainActivity extends ActionBarActivity {
+
+
+    public Drawable newDrawable;
+    public Drawable oldDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView v = (ImageView)findViewById(R.id.homeImage);
+        v.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            public void onSwipeLeft() {
+                // new
+                setImageSwipe(newDrawable);
+                //oldDrawable = curr;
+                //newDrawable = newd;
+            }
+
+            public void onSwipeRight() {
+                // old
+                setImageSwipe(oldDrawable);
+                //oldDrawable = oldd;
+                //newDrawable = curr;
+            }
+        });
+
     }
+
+    public void setImageSwipe(Drawable dr) {
+        //ImageView newiv = new ImageView(this);
+        //newiv.setImageResource(R.drawable.waterloo);
+        ImageView iv = (ImageView) findViewById(R.id.homeImage);
+        iv.setImageDrawable(dr);
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,6 +61,16 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    public void setImage(View v) {
+        //ImageView newiv = new ImageView(this);
+        //newiv.setImageResource(R.drawable.waterloo);
+        ImageView iv = (ImageView) findViewById(R.id.homeImage);
+        iv.setImageResource(R.drawable.waterloo);
+
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -36,3 +87,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
