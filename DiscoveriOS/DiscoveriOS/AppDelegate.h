db933aed346f0826c436d7acc8958fd70d99fa06
Joy Hsu <joyhsu0504@gmail.com>
2015-09-19 09:30:47 -0400

    added ios app

---
+++ DiscoveriOS/DiscoveriOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiscoveriOS
+//
+//  Created by Joy Hsu on 9/19/15.
+//  Copyright © 2015 Joy Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
