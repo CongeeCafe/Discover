5e4ade14e43070e596d7269f29c97f5a178b7410
CongeeCafe <k.zhou@hotmail.ca>
2015-09-19 10:18:03 -0400

    Begin work on EarthView

---
+++ DiscoverApp/app/src/main/java/com/discover/discoverapp/item/EarthImage.java
@@ -0,0 +1,10 @@
+package com.discover.discoverapp.item;
+
+/**
+ * Created by Kevin on 19/09/2015.
+ */
+public class EarthImage {
+
+
+
+}
---
+++ DiscoverApp/app/src/main/java/com/discover/discoverapp/net/Api.java
@@ -0,0 +1,16 @@
+package com.discover.discoverapp.net;
+
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by Kevin on 19/09/2015.
+ */
+public class Api {
+    interface WallpaperApi
+    {
+        @GET("/{id}.json")
+        EarthImage getWallpaper(@Path("id") String wallpaperId);
+    }
+
+}
